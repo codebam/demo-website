12e00c61699be1a0496973f808d617eb2e654c5c
Sean Behan <codebam@riseup.net>
2024-04-23 20:41:52 -0400

    add image gallery

---
+++ src/lib/component/Gallery.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let images: { alt: string, src: string }[];
+</script>
+
+<div id="gallery">
+    {#each images as image}
+        <img class="image" alt={image.alt} src={image.src} />
+    {/each}
+</div>
+
+<style>
+    #gallery {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(1, 12vw);
+        grid-gap: 15px;
+    }
+    .image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Gallery from '$lib/component/Gallery.svelte';
+    let images = [
+        {
+            alt: "alt text",
+            src: "https://i1.wp.com/blogedify.com/wp-content/uploads/2015/01/royalty-free-images.jpg?resize=1024%2C768"
+        },
+        {
+            alt: "alt text",
+            src: "https://i1.wp.com/blogedify.com/wp-content/uploads/2015/01/royalty-free-images.jpg?resize=1024%2C768"
+        }
+    ];
+</script>
+
+<Gallery images={images} />
+<h1>Demo Website</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
